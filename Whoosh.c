a2fac9979718b4ec988b8b7f3ce4defa14d9b7b0
hu-zhiling-540 <hu23z@mtholyoke.edu>
2018-03-05 22:26:33 -0500

    cd command; memory free

--- Whoosh.c
+++ Whoosh.c
@@ -5,32 +5,43 @@
 
 int const MAXARGS = 4;
 int const MAXLINE = 128; // maximum length of a line of input to the shell is 128 bytes
-int const MAXPATH = 100;
+int const MAXPATH = 100; // random number, can be disscussed later
 
-int parseline(char *cmdline, char **argv) {
+// by line, I mean each command
+int parseLine(char *cmdline, char **argv) {
   int count = 0;
-  char *separator = " \n\t"; /* Includes space, Enter, Tab */
+  char *separator = " \n\t"; // includes space, Enter, Tab
   argv[count] = strtok(cmdline, separator); // searches for the characters listed in separator
 
   while ((argv[count] != NULL) && (count+1 < MAXARGS)) {
     argv[++count] = strtok((char *) 0, separator);
   }
 
-  return count;
+  return count; // count of how many arguments in one command line
 
 }
 
+// given method for reporting error
 void reportError() {
   char error_message[30] = "An error has occurred\n";
   write(STDERR_FILENO, error_message, strlen(error_message));
 }
 
+int whoosh_cd(char *path){
+  if(path == NULL)
+    return chdir(getenv("HOME"));  // change the working directory to the path stored in HOME variable
+  else
+    return chdir(path);
+}
+
+// takes nothing, so we can simply call ./woosh
 int main(void){
 
   // int path;
 
-  char *buffer = malloc(sizeof(char) * MAXLINE);
+  char *buffer = malloc(sizeof(char) * (MAXLINE + 1)); // +1 takes null terminator into account; not sure
 
+  // simply check if memory allocation is successful
   if (!buffer) {
     fprintf(stderr, "allocation error\n");
     exit(EXIT_FAILURE);
@@ -41,29 +52,43 @@ int main(void){
     printf("whoosh> "); // print shell name
     fgets(buffer, MAXLINE, stdin); // reads in text; stdin is standard input
 
-    char** line_argv = malloc(sizeof(char*) * 128);
+    if (buffer == NULL) {// if user invokes EOF in stdin{
+      reportError();
+      continue;
+    }
+
+    char** line_argv = malloc(sizeof(char*) * MAXARGS);
     // call parseline to build argc/argv *
-    int line_argc = parseline(buffer, line_argv); // how many arguments a command takes
+    int line_argc = parseLine(buffer, line_argv); // how many arguments a command takes
 
     // check
-    printf("number of arguments taken in %d \n", line_argc);
+    printf("number of arguments for the cmd %d \n", line_argc);
+    for (int i = 0; i < line_argc; i++)
+      printf("print argv #%d: %s \n", i, line_argv[i]);
 
-    // buffer = strtok(buffer, "\n");
-    // int currLen = strlen(buffer);
     if (line_argc > MAXLINE){
       reportError();
       continue;
     }
 
-    if (strcmp(line_argv[0], "exit") == 0)
+    if (strcmp(line_argv[0], "cd") == 0)
+      whoosh_cd(line_argv[1]);
+
+    if (strcmp(line_argv[0], "exit") == 0)  {
+      free(line_argv);
+      free(buffer);
       exit(0);
+    }
+
     // If user hits enter key without a command
     // continue to loop again at the beginning
-    // if (line_argc != 0){
+    // if (line_argc == 0){
     //   // char* path = NULL;
     //   continue;
     // }
+
+    free(line_argv);
   }
-
-
+  free(buffer);
+  return 0;
 }
